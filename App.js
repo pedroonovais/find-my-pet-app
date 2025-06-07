b16395c3e85e7039d0f1ec4f882690363733a8ce
Pedro Novais <phenrique101007@gmail.com>
2025-06-07 20:35:56 -0300

    remove telas

--- App.js
+++ App.js
@@ -7,9 +7,7 @@ import LoginScreen from './src/components/screen/LoginScreen';
 import HomeScreen from './src/components/screen/HomeScreen';
 import ReportsScreen from './src/components/screen/Reports';
 import PerdidosScreen from './src/components/screen/PerdidosScreen';
-import AdocaoScreen from './src/components/screen/AdocaoScreen';
 import PerfilScreen from './src/components/screen/PerfilScreen';
-import AlertScreen from './src/components/screen/AlertScreen';
  
 const Stack = createNativeStackNavigator();
  
@@ -25,9 +23,7 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Reports" component={ReportsScreen} />
         <Stack.Screen name="Perdidos" component={PerdidosScreen} />
-        <Stack.Screen name="Adocao" component={AdocaoScreen} />
         <Stack.Screen name="Perfil" component={PerfilScreen} />
-        <Stack.Screen name="Alert" component={AlertScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
